c665ebd7a870db553b16cc187bed8af50a143761
Xinzhan <xinzhan.uva@gmail.com>
2022-03-10 00:32:12 -0500

    env script

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:./dict/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/dict/
 
 # generate ROOT dictionary
 echo "generating ROOT dictionaries..."
 cd dict
-rootcint -f StlDict.cxx ../include/Stl.h ../include/LinkDef.h
+# root 5
+rootcint -f StlDict.cxx -c ../include/Stl.h ../include/LinkDef.h
+# root 6
+#rootcint -f StlDict.cxx -c ../include/Stl.h ../include/LinkDef.h
+
 g++ -o libStlDict.so StlDict.cxx `root-config --cflags --libs` -shared -fPIC
 cd ..
